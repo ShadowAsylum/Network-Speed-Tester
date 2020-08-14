2d44a0ae57a00e98364e8e39341e62ad23de037d
Asylum <65395987+ShadowAsylum@users.noreply.github.com>
2020-08-14 17:30:40 +0200

    Updated JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-var i = "time";
-var iNumber = 0;
-var hour = {};
-            var hide = document.getElementById("hide");
+var Chart = new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{ 
+        data: [],
+        label: "Speed",
+        borderColor: "#3e95cd",
+        fill: true,
+            fontColor: 'white',
+      },
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'WiFi Speed'
+    },
+    layout: {
+            padding: {
+                left: 20,
+                right:25,
+                top: 20,
+                bottom: 0
+            },
+        scales: {
+      xAxes: [{
+        display: true,
+        gridLines: {
 
-//JUST AN EXAMPLE, PLEASE USE YOUR OWN PICTURE!
-var imageAddr = "http://www.kenrockwell.com/contax/images/g2/examples/31120037-5mb.jpg"; 
+        },
+      }],
+      yAxes: [{
+        display: true,
+        gridLines: {
+
+        },
+      }]
+    },
+
+    
+  }
+}})
+
+
+var postId = 1;
+
+var hide = document.getElementById("hide");
+
+var imageAddr = "https://ffwallpaper.com/wallup/skatepark/skatepark-3.jpg"; 
 var downloadSize = 4995374; //bytes
 
 function ShowProgressMessage(msg) {
@@ -26,7 +69,6 @@ function ShowProgressMessage(msg) {
 }
 
 setInterval(MeasureConnectionSpeed,60000);
-setInterval(InitiateSpeedDetection,60000);
 function InitiateSpeedDetection() {
 
     window.setTimeout(MeasureConnectionSpeed, 1);
@@ -68,63 +110,10 @@ hide.innerHTML="Updates Every Minute",
             speedMbps + " Mbps"
             
         ]);
-i = "time";
-iNumber = iNumber + 1;
-i = i + iNumber;
-console.log(i)
-var card_id = i;
-hour[card_id] = speedMbps;
 
-        
-  /*  
-for (var i = 0; i < 24; ++i) {
-    hour[i] = speedMbps;
+Chart.update();
+      Chart.data.labels.push(postId++);
+      Chart.data.datasets[0].data.push(speedMbps);
+      Chart.update();
 
-}
-*/
-
-new Chart(document.getElementById("line-chart"), {
-  type: 'line',
-  data: {
-    labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
-    datasets: [{ 
-        data: [hour.time1,hour.time2,hour.time3,hour.time4,hour.time5,hour.time6,hour.time7,hour.time8,hour.time9,hour.time10,hour.time11,hour.time12,hour.time13,hour.time14,hour.time15,hour.time16,hour.time17,hour.time18,hour.time19,hour.time20,hour.time21,hour.time22,hour.time23,hour.time24],
-        label: "Speed",
-        borderColor: "#3e95cd",
-        fill: true,
-            fontColor: 'white',
-      },
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'WiFi Speed'
-    },
-    layout: {
-            padding: {
-                left: 20,
-                right:25,
-                top: 20,
-                bottom: 0
-            },
-        scales: {
-      xAxes: [{
-        display: true,
-        gridLines: {
-
-        },
-      }],
-      yAxes: [{
-        display: true,
-        gridLines: {
-
-        },
-      }]
-    },
-
-    
-  }
-}})
-}
-}
+    }}
